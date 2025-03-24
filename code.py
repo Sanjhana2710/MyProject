d1cccc4ff7d4c8bdb5ecd463f4b7de47c4a85b70
Sanjhana2710 <sanjhanaw@gmail.com>
2025-03-24 13:26:09 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,6 @@
+import random
 
+def choose_word():
+    word = random.choice(["python", "hangman", "developer", "programming", "challenge", "computer", "science"])
+    print(f"Chosen word (for testing): {word}")  # Debugging print
+    return word
